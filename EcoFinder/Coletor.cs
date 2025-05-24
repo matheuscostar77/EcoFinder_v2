be09f7a1fc22d3663b646c018dd56afc771da462
matheuscostar77 <matheuscostarib@gmail.com>
2025-05-24 10:45:55 -0300

    Implementação da Classe Coletor e Classe Usuário

---
+++ EcoFinder/Coletor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcoFinder
+{
+    internal class Coletor : Pessoa
+    {
+        private int quantidadeColetas;
+
+        public Coletor()
+        {
+            quantidadeColetas = 0;
+        }
+
+        public void setQuantidadeColetas(int quantidadeColetas) 
+        {
+            this.quantidadeColetas= quantidadeColetas;
+        }
+
+        public int getQuantidadeColetas()
+        {
+            return quantidadeColetas;
+        }
+
+        public void verChamados()
+        {
+
+        }
+
+        public void reservarChamados()
+        {
+
+        }
+    }
+}
---
+++ EcoFinder/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoFinder
+{
+    internal class Usuario : Pessoa
+    {
+        Endereco endereco = new Endereco();
+
+        public void cadastrarEndereco()
+        {
+            
+        }
+
+        public void fazerChamado()
+        {
+
+        }
+
+        public void mudarEndereco()
+        {
+
+        }
+    }
+}
