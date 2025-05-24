3c976d516ccc3143eb8ed28123356fd6b2dbe625
clayalexssander <aclayver100@gmail.com>
2025-05-24 10:26:04 -0300

    Classe endereço

--- EcoFinder/Endereco.cs
+++ EcoFinder/Endereco.cs
@@ -8,70 +8,55 @@ namespace EcoFinder
 {
     internal class Endereco
     {
-        using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+        private string nomeBairro = "";
+        private string cidade = "";
+        private string nomeRua = "";
+        private int numeroCasa = 0;
 
-namespace EcoFinder
-    {
-        internal class Endereco
+        public Endereco(string nomeBairro, string cidade, string nomeRua, int numeroCasa)
         {
-            private string nomeBairro = "";
-            private string cidade = "";
-            private string nomeRua = "";
-            private int numeroCasa = 0;
+            this.nomeBairro = nomeBairro;
+            this.cidade = cidade;
+            this.nomeRua = nomeRua;
+            this.numeroCasa = numeroCasa;
+        }
+        public string getNomeBairro()
+        {
+            return nomeBairro;
+        }
+        public void setNomeBairro(string nomeBairro)
+        {
+            this.nomeBairro = nomeBairro;
+        }
 
-            public Endereco(string nomeBairro, string cidade, string nomeRua, int numeroCasa)
-            {
-                this.nomeBairro = nomeBairro;
-                this.cidade = cidade;
-                this.nomeRua = nomeRua;
-                this.numeroCasa = numeroCasa;
-            }
-            public string getNomeBairro()
-            {
-                return nomeBairro;
-            }
-            public void setNomeBairro(string nomeBairro)
-            {
-                this.nomeBairro = nomeBairro;
-            }
+        public string getCidade()
+        {
+            return cidade;
+        }
+        public void setCidade(string cidade)
+        {
+            this.cidade = cidade;
+        }
 
-            public string getCidade()
-            {
-                return cidade;
-            }
-            public void setCidade(string cidade)
-            {
-                this.cidade = cidade;
-            }
+        public string getNomeRua()
+        {
+            return nomeRua;
+        }
+        public void setNomeRua(string nomeRua)
+        {
+            this.nomeRua = nomeRua;
+        }
+        public int getNumeroCasa()
+        {
+            return numeroCasa;
+        }
 
-            public string getNomeRua()
-            {
-                return nomeRua;
-            }
-            public void setNomeRua(string nomeRua)
-            {
-                this.nomeRua = nomeRua;
-            }
 
-            public int getNumeroCasa()
-            {
-                return numeroCasa;
-            }
-            public void setNumeroCasa(int numeroCasa)
-            {
-                this.numeroCasa = numeroCasa;
-            }
-            
-            public void ExibirEndereco()
-            {
-                {
 
-                }
-            }
 
-}
+
+
+
+
+    }
 }
