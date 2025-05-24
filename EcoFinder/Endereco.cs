dd7e5729d36386affe7e0be328c4357776d0e04a
clayalexssander <aclayver100@gmail.com>
2025-05-24 10:23:11 -0300

    Classe endereço

---
+++ EcoFinder/Endereco.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoFinder
+{
+    internal class Endereco
+    {
+        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoFinder
+    {
+        internal class Endereco
+        {
+            private string nomeBairro = "";
+            private string cidade = "";
+            private string nomeRua = "";
+            private int numeroCasa = 0;
+
+            public Endereco(string nomeBairro, string cidade, string nomeRua, int numeroCasa)
+            {
+                this.nomeBairro = nomeBairro;
+                this.cidade = cidade;
+                this.nomeRua = nomeRua;
+                this.numeroCasa = numeroCasa;
+            }
+            public string getNomeBairro()
+            {
+                return nomeBairro;
+            }
+            public void setNomeBairro(string nomeBairro)
+            {
+                this.nomeBairro = nomeBairro;
+            }
+
+            public string getCidade()
+            {
+                return cidade;
+            }
+            public void setCidade(string cidade)
+            {
+                this.cidade = cidade;
+            }
+
+            public string getNomeRua()
+            {
+                return nomeRua;
+            }
+            public void setNomeRua(string nomeRua)
+            {
+                this.nomeRua = nomeRua;
+            }
+
+            public int getNumeroCasa()
+            {
+                return numeroCasa;
+            }
+            public void setNumeroCasa(int numeroCasa)
+            {
+                this.numeroCasa = numeroCasa;
+            }
+            
+            public void ExibirEndereco()
+            {
+                {
+
+                }
+            }
+
+}
+}
