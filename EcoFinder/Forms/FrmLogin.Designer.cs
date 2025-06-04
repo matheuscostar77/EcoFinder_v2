532f00e5bffc59a0b0e942acef81bc9aad46a06e
clayalexssander <aclayver100@gmail.com>
2025-06-03 23:49:57 -0300

    login update

--- EcoFinder/Forms/FrmLogin.Designer.cs
+++ EcoFinder/Forms/FrmLogin.Designer.cs
@@ -40,18 +40,19 @@
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.ForeColor = System.Drawing.Color.Black;
-            this.lblEmail.Location = new System.Drawing.Point(412, 117);
+            this.lblEmail.BackColor = System.Drawing.Color.White;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblEmail.ForeColor = System.Drawing.Color.OliveDrab;
+            this.lblEmail.Location = new System.Drawing.Point(441, 135);
             this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(34, 16);
+            this.lblEmail.Size = new System.Drawing.Size(54, 22);
             this.lblEmail.TabIndex = 1;
             this.lblEmail.Text = "Email";
             // 
             // tbxEmail
             // 
-            this.tbxEmail.Location = new System.Drawing.Point(427, 155);
+            this.tbxEmail.Location = new System.Drawing.Point(437, 165);
             this.tbxEmail.Margin = new System.Windows.Forms.Padding(2);
             this.tbxEmail.Name = "tbxEmail";
             this.tbxEmail.Size = new System.Drawing.Size(161, 20);
@@ -60,18 +61,19 @@
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSenha.ForeColor = System.Drawing.Color.Black;
-            this.lblSenha.Location = new System.Drawing.Point(412, 187);
+            this.lblSenha.BackColor = System.Drawing.Color.White;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSenha.ForeColor = System.Drawing.Color.OliveDrab;
+            this.lblSenha.Location = new System.Drawing.Point(442, 201);
             this.lblSenha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(36, 16);
+            this.lblSenha.Size = new System.Drawing.Size(58, 22);
             this.lblSenha.TabIndex = 3;
             this.lblSenha.Text = "Senha";
             // 
             // tbxSenha
             // 
-            this.tbxSenha.Location = new System.Drawing.Point(439, 216);
+            this.tbxSenha.Location = new System.Drawing.Point(439, 231);
             this.tbxSenha.Margin = new System.Windows.Forms.Padding(2);
             this.tbxSenha.Name = "tbxSenha";
             this.tbxSenha.Size = new System.Drawing.Size(161, 20);
@@ -82,10 +84,10 @@
             // 
             this.btnEntrar.BackColor = System.Drawing.Color.Green;
             this.btnEntrar.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnEntrar.Location = new System.Drawing.Point(466, 275);
+            this.btnEntrar.Location = new System.Drawing.Point(459, 271);
             this.btnEntrar.Margin = new System.Windows.Forms.Padding(2);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(109, 25);
+            this.btnEntrar.Size = new System.Drawing.Size(122, 27);
             this.btnEntrar.TabIndex = 6;
             this.btnEntrar.Text = "Entrar";
             this.btnEntrar.UseVisualStyleBackColor = false;
@@ -94,12 +96,13 @@
             // lblCriarConta
             // 
             this.lblCriarConta.AutoSize = true;
-            this.lblCriarConta.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCriarConta.LinkColor = System.Drawing.Color.DodgerBlue;
-            this.lblCriarConta.Location = new System.Drawing.Point(447, 321);
+            this.lblCriarConta.BackColor = System.Drawing.Color.White;
+            this.lblCriarConta.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCriarConta.LinkColor = System.Drawing.Color.DarkGreen;
+            this.lblCriarConta.Location = new System.Drawing.Point(442, 313);
             this.lblCriarConta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCriarConta.Name = "lblCriarConta";
-            this.lblCriarConta.Size = new System.Drawing.Size(141, 16);
+            this.lblCriarConta.Size = new System.Drawing.Size(166, 19);
             this.lblCriarConta.TabIndex = 9;
             this.lblCriarConta.TabStop = true;
             this.lblCriarConta.Text = "Não tem conta? Crie aqui";
@@ -109,7 +112,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(676, 387);
