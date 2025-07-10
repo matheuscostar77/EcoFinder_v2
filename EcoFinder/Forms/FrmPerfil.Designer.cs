1ce2f08562fb9672b1b267b3c22c33bd6c9c3073
matheuscostar77 <matheuscostarib@gmail.com>
2025-07-10 14:13:04 -0300

    Organização dos pacotes

--- EcoFinder/Forms/FrmPerfil.Designer.cs
+++ EcoFinder/Forms/FrmPerfil.Designer.cs
@@ -28,27 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnalterarEndereco = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbGenero = new System.Windows.Forms.TextBox();
+            this.txbCep = new System.Windows.Forms.TextBox();
+            this.txbRua = new System.Windows.Forms.TextBox();
+            this.txbBairro = new System.Windows.Forms.TextBox();
+            this.txbCidade = new System.Windows.Forms.TextBox();
+            this.txbEstado = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txbnumerocasa = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnAlterarPerfil = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnalterarEndereco
+            // 
+            this.btnalterarEndereco.Location = new System.Drawing.Point(516, 340);
+            this.btnalterarEndereco.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnalterarEndereco.Name = "btnalterarEndereco";
+            this.btnalterarEndereco.Size = new System.Drawing.Size(254, 94);
+            this.btnalterarEndereco.TabIndex = 2;
+            this.btnalterarEndereco.Text = "alterar endereco";
+            this.btnalterarEndereco.UseVisualStyleBackColor = true;
+            this.btnalterarEndereco.Click += new System.EventHandler(this.btnalterarEndereco_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(299, 211);
+            this.label1.Location = new System.Drawing.Point(249, 120);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "PERFIL";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 208);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cep:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 203);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 163);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 77);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nome:";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Enabled = false;
+            this.txbNome.Location = new System.Drawing.Point(315, 72);
+            this.txbNome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(148, 26);
+            this.txbNome.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(465, 322);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 10;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Enabled = false;
+            this.txbEmail.Location = new System.Drawing.Point(315, 115);
+            this.txbEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(148, 26);
+            this.txbEmail.TabIndex = 11;
+            // 
+            // txbGenero
+            // 
+            this.txbGenero.Enabled = false;
+            this.txbGenero.Location = new System.Drawing.Point(315, 155);
+            this.txbGenero.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbGenero.Name = "txbGenero";
+            this.txbGenero.Size = new System.Drawing.Size(148, 26);
+            this.txbGenero.TabIndex = 12;
+            // 
+            // txbCep
+            // 
+            this.txbCep.Enabled = false;
+            this.txbCep.Location = new System.Drawing.Point(315, 203);
+            this.txbCep.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbCep.Name = "txbCep";
+            this.txbCep.Size = new System.Drawing.Size(148, 26);
+            this.txbCep.TabIndex = 13;
+            // 
+            // txbRua
+            // 
+            this.txbRua.Enabled = false;
+            this.txbRua.Location = new System.Drawing.Point(584, 203);
+            this.txbRua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbRua.Name = "txbRua";
+            this.txbRua.Size = new System.Drawing.Size(274, 26);
+            this.txbRua.TabIndex = 22;
+            // 
+            // txbBairro
+            // 
+            this.txbBairro.Enabled = false;
+            this.txbBairro.Location = new System.Drawing.Point(584, 155);
+            this.txbBairro.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbBairro.Name = "txbBairro";
+            this.txbBairro.Size = new System.Drawing.Size(274, 26);
+            this.txbBairro.TabIndex = 21;
+            // 
+            // txbCidade
+            // 
+            this.txbCidade.Enabled = false;
+            this.txbCidade.Location = new System.Drawing.Point(585, 114);
+            this.txbCidade.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbCidade.Name = "txbCidade";
+            this.txbCidade.Size = new System.Drawing.Size(273, 26);
+            this.txbCidade.TabIndex = 20;
+            // 
+            // txbEstado
+            // 
+            this.txbEstado.Enabled = false;
+            this.txbEstado.Location = new System.Drawing.Point(585, 69);
+            this.txbEstado.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbEstado.Name = "txbEstado";
+            this.txbEstado.Size = new System.Drawing.Size(273, 26);
+            this.txbEstado.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(512, 74);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Estado:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(510, 163);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Bairro:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(512, 203);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 20);
+            this.label9.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(512, 208);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 20);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Rua:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(512, 118);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 20);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Cidade:";
+            // 
+            // txbnumerocasa
+            // 
+            this.txbnumerocasa.Enabled = false;
+            this.txbnumerocasa.Location = new System.Drawing.Point(582, 255);
+            this.txbnumerocasa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbnumerocasa.Name = "txbnumerocasa";
+            this.txbnumerocasa.Size = new System.Drawing.Size(276, 26);
+            this.txbnumerocasa.TabIndex = 25;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(512, 255);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 20);
+            this.label12.TabIndex = 24;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(512, 260);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(68, 20);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "n° Casa:";
+            // 
+            // btnAlterarPerfil
+            // 
+            this.btnAlterarPerfil.Location = new System.Drawing.Point(236, 340);
+            this.btnAlterarPerfil.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAlterarPerfil.Name = "btnAlterarPerfil";
+            this.btnAlterarPerfil.Size = new System.Drawing.Size(230, 94);
+            this.btnAlterarPerfil.TabIndex = 26;
+            this.btnAlterarPerfil.Text = "Alterar Perfil";
+            this.btnAlterarPerfil.UseVisualStyleBackColor = true;
+            this.btnAlterarPerfil.Click += new System.EventHandler(this.btnAlterarPerfil_Click);
             // 
             // FrmPerfil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1186, 692);
+            this.Controls.Add(this.btnAlterarPerfil);
+            this.Controls.Add(this.txbnumerocasa);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txbRua);
+            this.Controls.Add(this.txbBairro);
+            this.Controls.Add(this.txbCidade);
+            this.Controls.Add(this.txbEstado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txbCep);
+            this.Controls.Add(this.txbGenero);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnalterarEndereco);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmPerfil";
+            this.Text = "FrmPerfil";
+            this.Activated += new System.EventHandler(this.FrmPerfil_Activated);
             this.Load += new System.EventHandler(this.FrmPerfil_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -56,7 +324,29 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.Button btnalterarEndereco;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbGenero;
+        private System.Windows.Forms.TextBox txbCep;
+        private System.Windows.Forms.TextBox txbRua;
+        private System.Windows.Forms.TextBox txbBairro;
+        private System.Windows.Forms.TextBox txbCidade;
+        private System.Windows.Forms.TextBox txbEstado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txbnumerocasa;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnAlterarPerfil;
     }
 }
